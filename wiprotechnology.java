3346e48924ad25b7a90a35b202992796f0c2e1e8
vaishnaviyelnare <103577945+vaishnaviyelnare@users.noreply.github.com>
2022-10-03 00:39:10 +0530

    Add files via upload

---
+++ wiprotechnology.java
@@ -0,0 +1,19 @@
+class wiprotechnology
+{
+public static void main(String[]args)
+{
+int num1,num2;
+num1=Integer.parseInt(args[0]);
+num2=Integer.parseInt(args[1]);
+if(num1%10==num2%10)
+{
+	return true;
+}
+else
+{
+return false;
+}
+
+	
+}
+}
